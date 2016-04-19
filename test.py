3868ebab10f12bf11d9ab906ce3eb6ac797b00f1
clemens-hns <steve.clement@centurylink.com>
2016-04-19 11:21:13 -0400

    added array

--- test.py
+++ test.py
@@ -2,4 +2,9 @@
 
 print "testing"
 
+arr2 = [ 
+  {'Protocol': 'IP', 'Source_Address': '1.1.1.1', 'Source_mask': '/31', 'Destination_Address': '2.2.2.2', 'Destination_Mask': '/31'}
+    {'Protocol': 'IP', 'Source_Address': '1.1.1.2', 'Source_mask': '/31', 'Destination_Address': '2.2.2.3', 'Destination_Mask': '/31'} 
+    ]
 
+#arr2
